ca596abe313148d42090a5c7bad27230e4723061
Rohit Malla <P2779735@my365.dmu.ac.uk>
2024-06-05 06:00:55 +0100

    Properties and its test created

---
+++ ClassLibrary/clsCar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsCar
+    {
+        public clsCar()
+        {
+        }
+
+        public string Make { get; set; }
+
+        public bool Find(int vinNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
